7746c05481ff8c0ef9405d9df485f4d5ea9bf912
DAP <dlrayjr89@gmail.com>
2020-02-02 15:17:39 +0000

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import {Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
 import Header from "./components/Header.js";
 
 
 export default function App() {
   return (
-    <main>
-      <Header />
+    <main><Header />
+    <Route exact path='/characters' component={WelcomePage} />
+    <Route path='Characters' component={CharacterList} />
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+
+
+export default function SearchForm(props) {
+
+const [searching, setSearching] = useState([]);
+const handlechanges = event => {
+      setSearching(event.target.value);
+  }
+
+
+  const submittingHandler = e =>{
+      e.preventDefault();
+  }
+
+  const characherSearch = props.charachers.filers( character => {
+      return character.toLowerCase().indexOf(searching.toLowerCase()) !==1;
+  })
+  props.searchData(characherSearch);
+  console.log(characherSearch);
+
   return (
+
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit={submittingHandler}>
+         <input 
+         onChange={handlechanges}
+         type="text"
+         name="character"
+         placeholders="Search"
+         />
+     </form>
     </section>
   );
 }
