b2a9a0fada222b7245ce543c79b07fee3f5bf44a
DAP <dlrayjr89@gmail.com>
2020-02-05 21:22:11 +0000

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
 import Header from "./components/Header.js";
 
 
 export default function App() {
   return (
-    <main>
-      <Header />
-    </main>
+      <>
+    <Header />
+    <Route exact path='/' component={WelcomePage} />
+    <Route path='/characters' component={CharacterList} />
+    </>
+    
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,55 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard"
+import { Link } from "react-router-dom";
+import SearchForm from "./SearchForm";
+import styled from "styled-components";
+
+
+const StyledHeader = styled.h2`
+text-align: center;
+`
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  console.log(characters)
+  const [query, setQuery] = useState('');
+  console.log('I am query', characters)
+
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(res => {
+        // console.log(res.data.results)
+        const list = res.data.results.filter( item => {
+            return item.name.toLowerCase().includes(query.toLowerCase());
+        })
+        // const list = res.data.results;
+
+        setCharacters(list);
+       
+    })
+    .catch(error => {
+        console.log("an Error", error)
+    })
+  }, [query]);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <StyledHeader className="character-list">
+        <div>
+            <h2>Characters </h2>
+        </div>
+        <Link to={"/"}>
+        Home
+        </Link>
+
+        <SearchForm setQuery={setQuery} />
+        {characters.map( character => {
+            return <CharacterCard character={character} />
+        })}
+
+    </StyledHeader>
   );
 }
